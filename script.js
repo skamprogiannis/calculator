c1c9d6618ddbf32be0452b7421559e53796b929d
skamprogiannis <skamprogiannis@gmail.com>
2025-02-20 13:00:14 +0200

    Add basic operation logic

---
+++ script.js
@@ -0,0 +1,22 @@
+function multiplyadd(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(o, x, y) {
+  if (o === "+") return add(x, y);
+  if (o === "-") return subtract(x, y);
+  if (o === "*") return multiply(x, y);
+  if (o === "/") return divide(x, y);
+}
